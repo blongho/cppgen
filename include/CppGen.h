9ef9f2ac111436b819ea76d290258a753664af52
Longho Bernard <blongho02@gmail.com>
2025-04-27 23:13:33 +0200

    Prepare release 1.0.1
    Updates release script

--- include/CppGen.h
+++ include/CppGen.h
@@ -1,62 +1,61 @@
 #ifndef CPP_GEN_H
 #define CPP_GEN_H
-#include <string>
 #include <fstream>
+#include <string>
 
-namespace blongho {
-    class CppGen {
-    public:
-        explicit CppGen(std::string projectName = "demo",
-                        std::string projectType = "console",
-                        std::string buildSystem = "cmake",
-                        std::string cppStandard = "17",
-                        const bool &initGit = true);
+namespace blongho
+{
+class CppGen
+{
+   public:
+    explicit CppGen(std::string projectName = "demo", std::string projectType = "console",
+                    std::string buildSystem = "cmake", std::string cppStandard = "17", const bool &initGit = true);
 
-        ~CppGen() = default;
+    ~CppGen() = default;
 
-        CppGen(const CppGen &) = delete;
+    CppGen(const CppGen &) = delete;
 
-        CppGen &operator=(const CppGen &) = delete;
+    CppGen &operator=(const CppGen &) = delete;
 
-        CppGen(CppGen &&) = delete;
+    CppGen(CppGen &&) = delete;
 
-        CppGen &operator=(CppGen &&) = delete;
+    CppGen &operator=(CppGen &&) = delete;
 
-        /**
-         * Run the application
-         */
-        void run() const;
+    /**
+     * Run the application
+     */
+    void run() const;
 
-    private:
-        const std::string projectName;
-        const std::string projectType;
-        const std::string buildSystem;
-        const std::string cppStd;
-        const bool gitInit = false;
+   private:
+    const std::string projectName;
+    const std::string projectType;
+    const std::string buildSystem;
+    const std::string cppStd;
+    const bool        gitInit = false;
 
-        void create_directories() const;
+    void create_directories() const;
 
-        void generate_files() const;
+    void generate_files() const;
 
-        [[nodiscard]] std::string generate_main_cpp() const;
+    [[nodiscard]] std::string generate_main_cpp() const;
 
-        [[nodiscard]] std::string generate_cmake_lists() const;
+    [[nodiscard]] std::string generate_cmake_lists() const;
 
-        [[nodiscard]] std::string generate_test_cmake_lists() const;
+    [[nodiscard]] std::string generate_test_cmake_lists() const;
 
-        void list_directories() const;
+    void list_directories() const;
 
-    public:
-        [[nodiscard]] std::string get_project_name() const;
+   public:
+    [[nodiscard]] std::string get_project_name() const;
 
-        [[nodiscard]] std::string get_project_type() const;
+    [[nodiscard]] std::string get_project_type() const;
 
-        [[nodiscard]] std::string get_build_system() const;
+    [[nodiscard]] std::string get_build_system() const;
 
-        [[nodiscard]] std::string get_cpp_std() const;
+    [[nodiscard]] std::string get_cpp_std() const;
 
-        [[nodiscard]] bool is_git_init() const;
-    };
-}
+    [[nodiscard]] bool is_git_init() const;
+};
+}  // namespace blongho
 
-#endif //CPP_GEN_H
+#endif  // CPP_GEN_H
--- tests/test.cpp
+++ tests/test.cpp
@@ -2,26 +2,33 @@
 
 #include "CppGen.h"
 using namespace blongho;
-SCENARIO("CppGenTest", "[cppgen]") {
-    GIVEN("An the default initialization of cppgen") {
+SCENARIO("CppGenTest", "[cppgen]")
+{
+    GIVEN("An the default initialization of cppgen")
+    {
         const CppGen cppgen;
-        THEN("CppGen should be correctly initialized with project name demo, c++ std 17, and as console application") {
-            REQUIRE(cppgen.get_project_name() =="demo");
+        THEN(
+            "CppGen should be correctly initialized with project name demo, c++ std 17, and as "
+            "console application")
+        {
+            REQUIRE(cppgen.get_project_name() == "demo");
             REQUIRE(cppgen.get_cpp_std() == "17");
             REQUIRE(cppgen.get_build_system() == "cmake");
             REQUIRE(cppgen.get_project_type() == "console");
         }
     }
-    AND_GIVEN("An instance of CppGen with initialized parameters") {
-        THEN("It should fix the inputs and give correct values") {
+    AND_GIVEN("An instance of CppGen with initialized parameters")
+    {
+        THEN("It should fix the inputs and give correct values")
+        {
             const CppGen cppgen("Test Cpp Gen", "console", "cmake", "17");
-            REQUIRE(cppgen.get_project_name() =="test_cpp_gen");
+            REQUIRE(cppgen.get_project_name() == "test_cpp_gen");
             REQUIRE(cppgen.get_cpp_std() == "17");
             REQUIRE(cppgen.get_build_system() == "cmake");
             REQUIRE(cppgen.get_project_type() == "console");
 
             const CppGen cppgen2("test CPPGen", "console", "cmake", "C++17");
-            REQUIRE(cppgen2.get_project_name() =="test_cppgen");
+            REQUIRE(cppgen2.get_project_name() == "test_cppgen");
             REQUIRE(cppgen2.get_cpp_std() == "17");
             REQUIRE(cppgen2.get_build_system() == "cmake");
             REQUIRE(cppgen2.get_project_type() == "console");
